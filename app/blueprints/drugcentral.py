a42ff7c88cf57d282ca67ff59d1887d5a6869270
Christian Holladay <cholladay0816@gmail.com>
2023-08-17 11:58:07 -0500

    Searchable allowlist concept

--- app/blueprints/drugcentral.py
+++ app/blueprints/drugcentral.py
@@ -8,4 +8,9 @@ drugcentral = Blueprint('drugcentral', __name__, url_prefix="/drugcentral")
 @drugcentral.route('/<mol_id>')
 @drugcentral.route('/')
 def index(mol_id = None):
+    # Create an array of searchable fields
+    # These fields can be matched to allow for save 'AsIs' passing through the query
+    searchable = [
+        
+    ]
     return database.index('drugcentral', mol_id)
