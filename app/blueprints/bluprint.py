acc2d3111dcb6e547aaf3c88ae9506079a81cc16
Jack-42 <johnjack0987@gmail.com>
2024-07-12 12:00:39 -0600

    update version, remove test blueprint

--- app/blueprints/bluprint.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-test_bluprint = Blueprint("test_bluprint", __name__, url_prefix="/test_bluprint")
-
-
-@test_bluprint.route("/")
-def index():
-    x = "TEST"
-    return f"{x} : BluPrint registered!"
--- app/blueprints/version.py
+++ app/blueprints/version.py
@@ -1,29 +1,33 @@
 # blueprints/version.py
 # This is the master blueprint directory,
 # All new blueprints should be assigned here
-from blueprints.bluprint import test_bluprint
 from blueprints.hiers import hiers
+from flasgger import swag_from
 from flask import Blueprint, current_app
 
 # Set the route prefix based on version
-version = Blueprint("version", __name__, url_prefix="/api/v2")
+version = Blueprint("version", __name__, url_prefix="/api/v1")
 
 
 # Test Route
 @version.route("/")
+@swag_from(
+    {
+        "responses": {
+            200: {"description": "Returns the greeting."},
+            500: {
+                "description": "The app is misconfigured, APP_NAME config is missing or broken."
+            },
+        }
+    }
+)
 def hello():
-    """Hello World!
-      Returns a greeting to the user!
-      ---
-    responses:
-      200:
-        description: Returns the greeting.
-      500:
-        description: The app is misconfigured, APP_NAME config is missing or broken.
+    """
+    Hello World!
+    Returns a greeting to the user!
     """
     return f'{current_app.config.get("APP_NAME")} : Hello World!'
 
 
 # Register routes
-version.register_blueprint(test_bluprint)
 version.register_blueprint(hiers)
