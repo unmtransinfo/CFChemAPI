0a15920f935c539ba32d9b4108b02da42fca862a
Jack-42 <johnjack0987@gmail.com>
2024-07-17 15:33:28 -0600

    turn off debug

--- app/app.py
+++ app/app.py
@@ -16,4 +16,4 @@ swagger = Swagger(app)
 # Main loop
 if __name__ == "__main__":
     print(app.url_map)
-    app.run(host="0.0.0.0", port=app.config.get("APP_PORT"), debug=True)
+    app.run(host="0.0.0.0", port=app.config.get("APP_PORT"))
