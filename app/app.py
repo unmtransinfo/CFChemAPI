c82eae06c40845820dc696a332274545b8931de3
Jack-42 <johnjack0987@gmail.com>
2024-10-21 15:10:32 -0600

    run using gunicorn instead of default flask for WSGI

--- app/app.py
+++ app/app.py
@@ -4,16 +4,25 @@ from flasgger import Swagger
 from flask import Flask
 from flask_cors import CORS
 
-app = Flask(__name__)
-# Load config
-load_dotenv(".env")
-app.config.from_pyfile("config.py")
-# Register routes
-app.register_blueprint(version)
-CORS(app)
-swagger = Swagger(app)
 
-# Main loop
+def create_app():
+    app = Flask(__name__)
+
+    # Load config
+    load_dotenv(".env")
+    app.config.from_pyfile("config.py")
+
+    # Register routes
+    app.register_blueprint(version)
+
+    CORS(app)
+    swagger = Swagger(app)
+
+    return app
+
+
+app = create_app()
+
 if __name__ == "__main__":
     print(app.url_map)
-    app.run(host="0.0.0.0", port=app.config.get("APP_PORT"))
+    app.run(host="0.0.0.0", port=app.config.get("APP_PORT"), debug=True)
